9c4c256f524c8ef6174dc5702f4272430ca80315
Shihan Rehman <shihanrehman@gmail.com>
2016-03-14 03:35:39 -0400

    Deleted Comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,22 +4,22 @@ class ContactsController < ApplicationController
     @contacts = Contact.all
   end
 
-  # GET /professors/1
+  
   def show
     @contact = Contact.find(params[:id])
   end
 
-  # GET /professors/new
+  
   def new
     @contact = Contact.new
   end
 
-  # GET /professors/1/edit
+  
   def edit
     @contact = Contact.find(params[:id])
   end
 
-  # POST /professors
+  
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
@@ -29,7 +29,7 @@ class ContactsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /professors/1
+  
   def update
     @contact = Contact.find(params[:id])
     if @contact.update(contact_params)
@@ -39,7 +39,7 @@ class ContactsController < ApplicationController
     end
   end
 
-  # DELETE /professors/1
+  
   def destroy
     @contact = Contact.find(params[:id])
     @contact.destroy
